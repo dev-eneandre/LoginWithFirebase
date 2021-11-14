1ab0fa31307edc0bd61edb6b2831dc39c31033c4
Heracoder <iameneandre@gmail.com>
2021-11-15 00:31:24 +0100

    image updateppp

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,6 +19,6 @@ export default Home
 const Container = styled.div`
     min-height: 100vh;
     min-width : 375px;
-    background : linear-gradient(30deg, rgba(0, 0, 0, 2.5), rgba(0, 0, 0, 0.8)), url("/images/bg.jpg") center center / cover no-repeat fixed;
+    background : linear-gradient(30deg, rgba(0, 0, 0, 2.5), rgba(0, 0, 0, 0.8)), url("https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=kalen-emsley-Bkci_8qcdvQ-unsplash.jpg") center center / cover no-repeat fixed;
     color : #FFFFFF;
     `
